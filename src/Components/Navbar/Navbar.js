17f139d64f5de244cbc64ea6dfe26376120314f3
Rahat <58375065+islamrahat735@users.noreply.github.com>
2021-02-13 02:12:52 -0700

    made navbar but need to add our personalized content

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import './Navbar.css';
 import Nav from 'react-bootstrap/Nav'
+import reactDom from 'react-dom';
+import MenuItems from './MenuItems';
+import {Button} from '../Button'
+import './Navbar.css';
 
 
-function Navbar()
+class Navbar extends React.Component {
+  state = { clicked: false}
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked})
+  }
+
+  render()
+  {
+    return(
+        <nav className = "NavbarItems">
+          <h1 className = "navbar-logo">React<i className = "fab fa-react"></i></h1>
+          <div className = "menu-icon" onClick = {this.handleClick}>
+              <i className = {this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+          <ul className = {this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+            {MenuItems.map((item, index) =>{
+              return (
+                <li key = {index}>
+                    <a className = {item.cName} href = {item.url}>
+                    {item.title}
+                    </a>  
+                </li>
+              )
+            })}
+          </ul>
+          <Button>Sign Up</Button>
+        </nav>
+    )
+  }
+}
+/*function Navbar()
 {
     return(
 <div className = "navbar">
@@ -35,6 +70,6 @@ function Navbar()
 
     )
 }
-
+*/
 
 export default Navbar;
