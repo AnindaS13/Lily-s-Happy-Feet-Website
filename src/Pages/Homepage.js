5e5e03a37132ada049f70cd623ed1fd779cbdf45
AnindaS13 <73033758+AnindaS13@users.noreply.github.com>
2021-02-12 22:00:52 -0700

    Front page done

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -10,9 +10,12 @@ function Homepage()
             <Navbar />
             <div className = "backImage">
 
-                <h1>Lily's Happy Feet</h1>
+                <h1 className = "title">Lily's Happy Feet</h1>
+                <h2 className = "subtitle"> A Mobile Footcare Clinic </h2>
+                <h3 className = "sentence">loving and compassionate hand and foot care since 2013</h3>
             </div>
             <h1> Testing</h1>
+         
         </div>
     );
 }
