e4a7260528701803635d0ec6795449ec74dbf89d
AnindaS13 <73033758+AnindaS13@users.noreply.github.com>
2021-02-12 23:16:53 -0700

    Newer Homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Homepage.css';
-import Navbar from "../Components/Navbar/Navbar"
+import Navbar from "../Components/Navbar/Navbar";
+import Table from 'react-bootstrap/Table';
 
 
 function Homepage()
@@ -14,7 +15,20 @@ function Homepage()
                 <h2 className = "subtitle"> A Mobile Footcare Clinic </h2>
                 <h3 className = "sentence">loving and compassionate hand and foot care since 2013</h3>
             </div>
-            <h1> Testing</h1>
+            
+            <div>
+            <h4 style ={{marginLeft: 210, paddingTop: 50, textDecoration: "underline", fontFamily: "Verdana", fontWeight:900}}>BENEFITS</h4>
+            <h1 style ={{marginLeft: 210, paddingTop: 25, fontFamily: "Arial", fontSize: 60}}>Taking Care of You Is Our Priority</h1>
+            <p style ={{marginLeft: 210, paddingTop: 40, marginRight: 600, fontSize: 30, fontFamily: "Arial"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mattis dictumst praesent volutpat vitae odio nisl, pulvinar. 
+            Viverra tellus auctor nibh nulla pellentesque malesuada. 
+            </p>
+            
+            <div className= "rectangle"></div>
+            <div className= "rectangle2"></div>
+            <div className= "rectangle3"></div>
+
+
+            </div>
          
         </div>
     );
